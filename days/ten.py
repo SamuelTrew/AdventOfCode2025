6060c3dea371840c704bd38672e2bf9339bf061d
Samuel Trew <samuel.trew@outlook.com>
2025-12-07 21:55:10 +0000

    Initialised with base python setup with timing and days

---
+++ days/ten.py
@@ -0,0 +1,18 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def get_lines():
+   with open("../input/1.txt") as f:
+      return f.readlines()
+
+def part1():
+   lines = get_lines()
+
+   return 0
+
+def part2():
+   lines = get_lines()
+
+   return 0
---
+++ main.py
@@ -0,0 +1,21 @@
+from days.one import part1, part2
+import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+    t1 = time.perf_counter()
+    res1 = part1()
+    t2 = time.perf_counter()
+    logger.info(f"Part 1 took {round(t2-t1, 4)} seconds. Result = {res1}")
+
+    t3 = time.perf_counter()
+    res2 = part2()
+    t4 = time.perf_counter()
+    logger.info(f"Part 1 took {round(t4-t3, 4)} seconds. Result = {res2}")
+
+
+if __name__ == "__main__":
+    main()
