b438e5fd1512cd6ffdbbfe6e0cb6b555971ed00d
Samuel Trew <samuel.trew@outlook.com>
2025-12-09 12:54:34 +0000

    Finished Day 4

--- days/four.py
+++ days/four.py
@@ -4,15 +4,71 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def get_lines():
-   with open("input/1.txt") as f:
+   with open("input/4.txt") as f:
       return f.readlines()
 
 def part1():
    lines = get_lines()
+   rolls = [[x for x in line if x != "\n"] for line in lines]
+   res = 0
 
-   return 0
+   for i in range(len(rolls)):
+      for j in range(len(rolls[i])):
+         roll = rolls[i][j]
+         if roll != "@":
+            continue
+         count = -1 # -1 to handle k==0 and l==0
 
+         for k in range(-1, 2):
+            idx = i + k
+            if idx < 0 or idx >= len(rolls):
+               continue
+            for l in range(-1, 2):
+               jdx = j + l
+               if jdx < 0 or jdx >= len(rolls[idx]):
+                  continue
+               surrounding_roll = rolls[i+k][j+l]
+               if surrounding_roll == "@":
+                  count += 1
+
+         if count < 4:
+            res += 1
+
+   return res
+
+# Not going to generalise the code as I want to alter the map
+# while iterating through for perf gain
 def part2():
    lines = get_lines()
+   rolls = [[x for x in line if x != "\n"] for line in lines]
+   prev_res = -1
+   res = 0
 
-   return 0
+   while res != prev_res:
+      prev_res = res
+
+      # This is just copied from part one
+      for i in range(len(rolls)):
+         for j in range(len(rolls[i])):
+            roll = rolls[i][j]
+            if roll != "@":
+               continue
+            count = -1 # -1 to handle k==0 and l==0
+
+            for k in range(-1, 2):
+               idx = i + k
+               if idx < 0 or idx >= len(rolls):
+                  continue
+               for l in range(-1, 2):
+                  jdx = j + l
+                  if jdx < 0 or jdx >= len(rolls[idx]):
+                     continue
+                  surrounding_roll = rolls[i+k][j+l]
+                  if surrounding_roll == "@":
+                     count += 1
+
+            if count < 4:
+               res += 1
+               rolls[i][j] = "."
+
+   return res
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from days.three import part1, part2
+from days.four import part1, part2
 import time
 import logging
 
